ad930edacf2bb87ca19285c2db9716a1f0a88551
Rafał Harabień <rafalh1992@o2.pl>
2013-03-03 16:52:42 +0000

    Improve code to support onPlayerWinDD event Reformat code Add sound files from race gamemode

--- race_audio/audio_c.lua
+++ race_audio/audio_c.lua
@@ -1,33 +1,29 @@
 local g_Root = getRootElement()
 local g_ResRoot = getResourceRootElement(getThisResource())
-
-local soundVolume = 0.5
-
-function playAudio(filename)
-	-- outputDebugString("play sound "..filename)
-	local sound = playSound("audio/"..filename)
-	setSoundVolume(sound, soundVolume)
-end
+local g_SoundVolume = 0.5
 
 addEvent("playClientAudio", true)
-addEventHandler("playClientAudio", g_Root, playAudio)
-
-addEventHandler('onClientResourceStart', g_ResRoot,
-	function()
-		playAudio("raceon.mp3")
-	end
-)
-
 addEvent("onClientPlayerOutOfTime", true)
-addEventHandler('onClientPlayerOutOfTime', g_Root,
-	function()
-		playAudio("timesup.mp3")
-	end
-)
 
-addCommandHandler("soundvolume",
-    function(cmd, value)
-		soundVolume = value/100
-		outputConsole("set sound volume to "..value.."%")
-    end
-)
+local function playAudio(filename)
+	local sound = playSound("audio/"..filename)
+	setSoundVolume(sound, g_SoundVolume)
+end
+
+local function onResStart()
+	playAudio("raceon.mp3")
+end
+
+local function onClientPlayerOutOfTime()
+	playAudio("timesup.mp3")
+end
+
+local function onSoundVolumeCmd(cmd, value)
+	g_SoundVolume = value/100
+	outputConsole("set sound volume to "..value.."%")
+end
+
+addEventHandler("playClientAudio", g_Root, playAudio)
+addEventHandler("onClientResourceStart", g_ResRoot, onResStart)
+addEventHandler("onClientPlayerOutOfTime", g_Root, onClientPlayerOutOfTime)
+addCommandHandler("soundvolume", onSoundVolumeCmd, false, false)
--- race_audio/audio_s.lua
+++ race_audio/audio_s.lua
@@ -1,78 +1,47 @@
 local g_Root = getRootElement()
+local g_MapMode = ""
+local g_KillReq = false
 
-local mapMode = ""
+addEvent("onMapStarting")
+addEvent("onRequestKillPlayer", true)
+addEvent("onPlayerToptimeImprovement")
 
-function playAudio(player, filename)
+local function playAudio(player, filename)
 	-- outputDebugString("play sound "..filename.." for "..getPlayerName(player))
 	triggerClientEvent(player, "playClientAudio", getRootElement(), filename)
 end
 
-addEvent("onMapStarting")
-addEventHandler('onMapStarting', g_Root,
-	function(mapInfo)
-		mapMode = mapInfo.modename
-	end
-)
-
-addEventHandler('onPlayerQuit', g_Root,
-    function()
-        if mapMode == "Destruction derby" and getElementData(source,"state") == "alive" then
-			local alivePlayers = getAlivePlayers()
-            for i,player in ipairs(alivePlayers) do
-                if player == source then
-                    table.remove(alivePlayers, i)
-                end
-            end
-            if #alivePlayers == 1 then
-                playAudio(alivePlayers[1], "jobcomplete.mp3")
-            end
-		end
-    end
-)
-
-addEventHandler('onPlayerWasted', g_Root,
-	function()
-		if mapMode == "Destruction derby" then
-			local alivePlayers = getAlivePlayers()
-			if #alivePlayers >= 1 then
-				playAudio(source, "jobfail.mp3")
-				if #alivePlayers == 1 then
-					playAudio(alivePlayers[1], "jobcomplete.mp3")
-				end
-			end
-		else
-			if p_Killed then
-				p_Killed = false
-			else
-				playAudio(source, "wasted.mp3")
-			end
-		end
-	end
-)
-
-addEvent('onRequestKillPlayer', true)
-addEventHandler('onRequestKillPlayer', g_Root,
-    function()
-		p_Killed = true
-    end
-)
-
-addEvent("onPlayerToptimeImprovement")
-addEventHandler("onPlayerToptimeImprovement", g_Root,
-	function(newPos, newTime, oldPos, oldTime, displayTopCount, validEntryCount)
-		if newPos <= displayTopCount and newPos <= validEntryCount then
-			playAudio(source, "nicework.mp3")
-		end
-	end
-)
-
-local _getAlivePlayers = getAlivePlayers
-function getAlivePlayers(player)
-	local result = {}
-	for _,player in ipairs(_getAlivePlayers()) do
-		if getElementData(player, "state") == "alive" then
-			table.insert(result, player)
-		end
-	end
-	return result
+local function onMapStarting(mapInfo)
+	g_MapMode = mapInfo.modename
 end
+
+local function onPlayerWasted()
+	if g_MapMode == "Destruction derby" then
+		playAudio(source, "jobfail.mp3")
+	elseif(g_KillReq) then
+		g_KillReq = false
+	else
+		playAudio(source, "wasted.mp3")
+	end
+end
+
+local function onRequestKillPlayer()
+	g_KillReq = true
+end
+
+local function onPlayerToptimeImprovement(newPos, newTime, oldPos, oldTime, displayTopCount, validEntryCount)
+	--TODO: Fix this event
+	if newPos <= displayTopCount and newPos <= validEntryCount then
+		playAudio(source, "nicework.mp3")
+	end
+end
+
+local function onPlayerWinDD()
+	playAudio(source, "jobcomplete.mp3")
+end
+
+addEventHandler('onMapStarting', g_Root, onMapStarting)
+addEventHandler('onPlayerWasted', g_Root, onPlayerWasted)
+addEventHandler('onRequestKillPlayer', g_Root, onRequestKillPlayer)
+addEventHandler("onPlayerToptimeImprovement", g_Root, onPlayerToptimeImprovement)
+addEventHandler("onPlayerWinDD", g_Root, onPlayerWinDD)
