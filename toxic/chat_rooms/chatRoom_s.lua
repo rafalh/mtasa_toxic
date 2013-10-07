555519ca119da3b7307837f80e752f1a3c5f869a
Rafał Harabień <rafalh1992@o2.pl>
2013-10-07 18:40:06 +0000

    Move chatext resource to toxic resource

---
+++ toxic/chat_rooms/chatRoom_s.lua
@@ -0,0 +1,136 @@
+---------------------
+-- Local variables --
+---------------------
+
+local g_Root = getRootElement()
+
+-------------------
+-- Custom events --
+-------------------
+
+addEvent("chatext.onMsg", true)
+addEvent("chatext.onPlayerChat")
+
+ChatRoom = {}
+ChatRoom.__mt = {__index = ChatRoom}
+ChatRoom.idToRoom = {}
+
+function ChatRoom:onPlayerMsg(player, msg)
+	--outputDebugString("[chatext] Chat msg: "..self.id.." player "..getPlayerName(player), 3)
+	
+	msg = tostring(msg)
+	if(msg == "") then return end
+	if(self.info.checkAccess and not self.info.checkAccess(player)) then return end
+	
+	if(getElementType(player) == 'player' and isPlayerMuted(player)) then
+		outputChatBox(self.info.cmd..": You are muted!", player, 255, 128, 0)
+		return
+	end
+	
+	msg = utfSub(msg, 1, 128)
+	
+	local recipients = self.info.getPlayers(player)
+	local playerName = getPlayerName(player)
+	local chatPrefix = self.info.chatPrefix
+	if(type(chatPrefix) == "function") then chatPrefix = chatPrefix(player) end
+	local logPrefix = self.info.logPrefix
+	if(type(logPrefix) == "function") then logPrefix = logPrefix(player) end
+	
+	if(utfSub(msg, 1, 1) ~= "/") then
+		local pdata = Player.fromEl(player)
+		local punishment = false
+		if(CsProcessMsg) then
+			msg, punishment = CsProcessMsg(msg)
+			if(not msg) then
+				-- Message has been blocked
+				CsPunish(pdata, punishment)
+				return
+			end
+		end
+		
+		local str = chatPrefix..playerName..": #EBDDB2"..msg
+		local r, g, b 
+		if(getElementType(player) == 'player') then
+			r, g, b = getPlayerNametagColor(player)
+		else
+			r, g, b = 255, 0, 255
+		end
+		--local foundSender = false
+		for i, recipient in ipairs(recipients) do
+			outputChatBox(str, recipient, r, g, b, true)
+			--if(player == recipient) then foundSender = true end
+		end
+		--assert(foundSender)
+		outputServerLog(logPrefix..playerName..": "..msg)
+		
+		if(punishment) then
+			CsPunish(pdata, punishment)
+		end
+	end
+	
+	local rafalhRes = getResourceFromName("toxic")
+	if(rafalhRes and getResourceState(rafalhRes) == "running") then
+		call(rafalhRes, "parseCommand", msg, player, recipients, chatPrefix)
+	end
+end
+
+function ChatRoom.create(info)
+	local self = setmetatable({}, ChatRoom.__mt)
+	self.info = info
+	self.id = info.id
+	ChatRoom.idToRoom[info.id] = self
+	return self
+end
+
+--------------------------------
+-- Local function definitions --
+--------------------------------
+
+local function onMsg(roomId, msg)
+	local self = ChatRoom.idToRoom[roomId]
+	assert(self)
+	self:onPlayerMsg(client, msg)
+end
+
+local function onConsole(msg)
+	for id, room in pairs(ChatRoom.idToRoom) do
+		local cmdLen = room.info.cmd:len()
+		if(msg:sub(1, cmdLen + 1) == room.info.cmd.." ") then
+			room:onPlayerMsg(source, msg:sub(1 + cmdLen + 1))
+		end
+	end
+end
+
+addEvent("chatext.onModVerified", true)
+addEvent("chatext.onReady", true)
+
+local function onPlayerReady()
+	g_ReadyPlayers[client] = true
+	if(hasObjectPermissionTo(client, "resource.rafalh_modchat", false)) then
+		triggerClientEvent(client, "chatext.onModVerified", resourceRoot)
+	end
+end
+
+local function onModVerified()
+	g_ModRoom:enable()
+end
+
+local function onPlayerLogin()
+	if(g_ReadyPlayers[source] and hasObjectPermissionTo(source, "resource.rafalh_modchat", false)) then
+		triggerClientEvent(source, "chatext.onModVerified", resourceRoot)
+	end
+end
+
+local function onPlayerQuit()
+	g_ReadyPlayers[source] = nil
+end
+
+
+------------
+-- Events --
+------------
+
+addInitFunc(function()
+	addEventHandler("chatext.onMsg", resourceRoot, onMsg)
+	addEventHandler("onConsole", root, onConsole)
+end)
---
+++ toxic/chat_rooms/rooms.lua
@@ -0,0 +1,89 @@
+local g_ReadyPlayers = {}
+
+local g_ModRoom = ChatRoom.create{
+	id = "mod",
+	key = "u",
+	inputPrefix = "Modsay:",
+	chatPrefix = "(MOD) ",
+	logPrefix = "MODSAY: ",
+	cmd = "modsay",
+	checkAccess = function(sender)
+		return hasObjectPermissionTo(sender, "resource.rafalh_modchat", false)
+	end,
+	disabled = true,
+	getPlayers = function(sender)
+		local recipients = {}
+		for i, player in ipairs(getElementsByType("player")) do
+			if(hasObjectPermissionTo(player, "resource.rafalh_modchat", false)) then
+				table.insert(recipients, player)
+			end
+		end
+		return recipients
+	end
+}
+
+local g_LangRoom = ChatRoom.create{
+	id = "lang",
+	key = "l",
+	inputPrefix = function(player)
+		local lang = getElementData(player, "country") or "EN"
+		return lang..":"
+	end,
+	chatPrefix = function(player)
+		local lang = getElementData(player, "country") or "EN"
+		return "("..lang..") "
+	end,
+	logPrefix = function(player)
+		local lang = getElementData(player, "country") or "EN"
+		return lang.."SAY: "
+	end,
+	cmd = "langsay",
+	getPlayers = function(sender)
+		local lang = getElementData(sender, "country")
+		local recipients = {}
+		for i, player in ipairs(getElementsByType("player")) do
+			if(getElementData(player, "country") == lang) then
+				table.insert(recipients, player)
+			--else
+			--	assert(player ~= sender)
+			end
+		end
+		
+		return recipients
+	end
+}
+
+addEvent("chatext.onModVerified", true)
+addEvent("chatext.onReady", true)
+
+if(triggerClientEvent) then
+	local function onPlayerReady()
+		g_ReadyPlayers[client] = true
+		if(hasObjectPermissionTo(client, "resource.rafalh_modchat", false)) then
+			triggerClientEvent(client, "chatext.onModVerified", resourceRoot)
+		end
+	end
+	
+	local function onPlayerLogin()
+		if(g_ReadyPlayers[source] and hasObjectPermissionTo(source, "resource.rafalh_modchat", false)) then
+			triggerClientEvent(source, "chatext.onModVerified", resourceRoot)
+		end
+	end
+	
+	local function onPlayerQuit()
+		g_ReadyPlayers[source] = nil
+	end
+	
+	addInitFunc(function()
+		addEventHandler("chatext.onReady", resourceRoot, onPlayerReady)
+		addEventHandler("onPlayerLogin", root, onPlayerLogin)
+		addEventHandler("onPlayerQuit", root, onPlayerQuit)
+	end)
+else
+	local function onModVerified()
+		g_ModRoom:enable()
+	end
+	
+	addEventHandler("chatext.onModVerified", resourceRoot, onModVerified)
+end
+
