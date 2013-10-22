d6c78e50896171305bc5c6d021ca2b09cf697d89
Rafał Harabień <rafalh1992@o2.pl>
2013-10-22 23:00:13 +0000

    Add option not to use helpmanager resource

--- toxic/gui/help_c.lua
+++ toxic/gui/help_c.lua
@@ -3,19 +3,18 @@
 
 -- VARIABLES
 local g_Commands
-local g_HelpTab
-local g_SearchEdit
-local g_CmdList
+local g_LoadingCmdList = false
+local g_GuiParent, g_SearchEdit, g_CmdList
 local g_HelpMgrRes = Resource('helpmanager')
 
 -- FUNCTIONS
-local function HlpUpdateList ()
+local function HlpUpdateList()
 	if(not g_CmdList or not g_Commands) then return end
 	
 	guiGridListClear(g_CmdList)
 	
 	local text = guiGetText(g_SearchEdit)
-	local pattern = text:lower ()
+	local pattern = text:lower()
 	
 	for i, cmd in ipairs(g_Commands) do
 		local cmdNameLower = cmd[1]:lower()
@@ -33,54 +32,96 @@ local function HlpCmdList(commands)
 	HlpUpdateList()
 end
 
-local function HlpCreateGui()
-	local w, h = guiGetSize(g_HelpTab, false)
-	local y = 10
-	
-	local title = guiCreateLabel(10, y, w - 20, 15, "Controls", false, g_HelpTab)
+local function HlpCreateGui(parent, x, y, w, h)
+	local title = guiCreateLabel(x, y, w, 15, "Controls", false, parent)
 	guiSetFont(title, 'default-bold-small')
 	local userPanelKey = getKeyBoundToCommand('UserPanel')
 	local statsPanelKey = getKeyBoundToCommand('StatsPanel')
 	local invKey = getKeyBoundToCommand('UserInventory')
 	local mapInfoKey = getKeyBoundToCommand('MapInfoGui')
-	guiCreateLabel(10, y + 1*15, w - 20, 15, MuiGetMsg("Press %s to show Statistics Panel."):format(statsPanelKey), false, g_HelpTab)
-	guiCreateLabel(10, y + 2*15, w - 20, 15, MuiGetMsg("Press %s to show User Panel."):format(userPanelKey), false, g_HelpTab)
-	guiCreateLabel(10, y + 3*15, w - 20, 15, MuiGetMsg("Press %s to show User Items."):format(invKey), false, g_HelpTab)
-	guiCreateLabel(10, y + 4*15, w - 20, 15, MuiGetMsg("Press %s to show Map Info."):format(mapInfoKey), false, g_HelpTab)
+	guiCreateLabel(x, y + 1*15, w, 15, MuiGetMsg("Press %s to show Statistics Panel."):format(statsPanelKey), false, parent)
+	guiCreateLabel(x, y + 2*15, w, 15, MuiGetMsg("Press %s to show User Panel."):format(userPanelKey), false, parent)
+	guiCreateLabel(x, y + 3*15, w, 15, MuiGetMsg("Press %s to show User Items."):format(invKey), false, parent)
+	guiCreateLabel(x, y + 4*15, w, 15, MuiGetMsg("Press %s to show Map Info."):format(mapInfoKey), false, parent)
 	y = y + 5*15 + 10
 	
-	local title = guiCreateLabel(10, y, w - 20, 15, "Commands", false, g_HelpTab)
+	local title = guiCreateLabel(x, y, w, 15, "Commands", false, parent)
 	guiSetFont(title, 'default-bold-small')
-	guiCreateLabel(10, y + 16, 50, 15, "Search:", false, g_HelpTab)
-	g_SearchEdit = guiCreateEdit(60, y + 15, 150, 20, '', false, g_HelpTab)
+	guiCreateLabel(x, y + 16, 50, 15, "Search:", false, parent)
+	g_SearchEdit = guiCreateEdit(x + 50, y + 15, 150, 20, '', false, parent)
 	addEventHandler('onClientGUIChanged', g_SearchEdit, HlpUpdateList, false)
-	g_CmdList = guiCreateGridList(10, y + 40, w - 20, h - (y + 40) - 10, false, g_HelpTab)
+	g_CmdList = guiCreateGridList(x, y + 40, w, h - (y + 40), false, parent)
 	guiGridListAddColumn(g_CmdList, "Command", 0.2)
 	guiGridListAddColumn(g_CmdList, "Description", 0.7)
 end
 
-local function HlpTabShown()
-	if(not g_CmdList) then
-		HlpCreateGui()
-	end
-	if(not g_Commands) then
+local function HlpLoadCmdList()
+	if(not g_Commands and not g_LoadingCmdList) then
+		g_LoadingCmdList = true
 		RPC('CmdMgr.getCommandsForHelp'):onResult(HlpCmdList):exec()
 	end
 end
 
+local function HlpTabShown()
+	if(not g_CmdList) then
+		local w, h = guiGetSize(g_GuiParent, false)
+		HlpCreateGui(g_GuiParent, 10, 10, w - 20, h - 20)
+	end
+	HlpLoadCmdList()
+end
+
+local function HlpCloseWnd()
+	guiSetVisible(g_GuiParent, false)
+	showCursor(false)
+end
+
+local function HlpCreateWnd()
+	local w, h = 500, 400
+	local x, y = (g_ScreenSize[1]-w)/2, (g_ScreenSize[2]-h)/2
+	g_GuiParent = guiCreateWindow(x, y, w, h, "Toxic Help", false)
+	guiWindowSetSizable(g_GuiParent, false)
+	
+	local closeBtn = guiCreateButton(w - 90, h - 35, 80, 25, "Close", false, g_GuiParent)
+	addEventHandler('onClientGUIClick', closeBtn, HlpCloseWnd, false)
+	
+	HlpCreateGui(g_GuiParent, 10, 25, w - 20, h - 45)
+	HlpLoadCmdList()
+end
+
+local function HlpOpenWnd()
+	if(not g_GuiParent) then
+		HlpCreateWnd()
+	else
+		guiSetVisible(g_GuiParent, true)
+	end
+	
+	showCursor(true)
+	guiBringToFront(g_GuiParent)
+end
+
+local function HlpToggleWnd()
+	if(g_GuiParent and guiGetVisible(g_GuiParent)) then
+		HlpCloseWnd()
+	else
+		HlpOpenWnd()
+	end
+end
+
 local function HlpInit()
-	if(g_HelpMgrRes:isReady()) then
-		g_HelpTab = g_HelpMgrRes:call('addHelpTab', getThisResource(), true)
-		guiSetText(g_HelpTab, "Rafalh Scripts System")
+	if(Settings.helpmgr and g_HelpMgrRes:isReady()) then
+		g_GuiParent = g_HelpMgrRes:call('addHelpTab', getThisResource(), true)
+		guiSetText(g_GuiParent, "Rafalh Scripts System")
 		
-		addEventHandler('onClientGUITabSwitched', g_HelpTab, HlpTabShown)
-		local tabPanel = getElementParent(g_HelpTab)
+		addEventHandler('onClientGUITabSwitched', g_GuiParent, HlpTabShown)
+		local tabPanel = getElementParent(g_GuiParent)
 		if(not tabPanel) then outputDebugString('No tab panel found', 2)
 		else guiGetSelectedTab(tabPanel) end
 		
-		if(guiGetSelectedTab(tabPanel) == g_HelpTab) then
+		if(guiGetSelectedTab(tabPanel) == g_GuiParent) then
 			HlpTabShown()
 		end
+	else
+		bindKey('F9', 'down', HlpToggleWnd)
 	end
 end
 
