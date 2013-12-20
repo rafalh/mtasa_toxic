300c885425c2eadd9d9aec4f46d8f1ac76fe0ce7
Rafał Harabień <rafalh1992@o2.pl>
2013-12-20 22:15:14 +0000

    Fix /mergeaccounts Add /mergeaccountsoffline

--- toxic/commands/admin_s.lua
+++ toxic/commands/admin_s.lua
@@ -231,156 +231,187 @@ CmdMgr.register{
 	end
 }
 
+local function mergeAccounts(destId, srcId)
+	-- get statistics for id
+	local srcAccountData = AccountData.create(srcId)
+	local srcData = srcAccountData:getTbl()
+	if(not srcData) then
+		return 'Cannot find source account in database!'
+	end
+	
+	local destAccountData = AccountData.create(destId)
+	if(not destAccountData:getTbl()) then
+		return 'Cannot find destination account in database!'
+	end
+	
+	--[[if(srcData.first_visit > destAccountData.first_visit) then
+		return 'Account for merge is newer'
+	end]]
+	
+	-- update stats
+	local newData = {}
+	newData.cash = destAccountData.cash + srcData.cash
+	newData.points = destAccountData.points + srcData.points
+	newData.bidlvl = math.max(srcData.bidlvl, destAccountData.bidlvl)
+	newData.time_here = destAccountData.time_here + srcData.time_here
+	newData.first_visit = srcData.first_visit
+	if(destAccountData.email == '') then
+		newData.email = srcData.email
+	end
+	
+	-- Statistics
+	newData.exploded = destAccountData.exploded + srcData.exploded
+	newData.drowned = destAccountData.drowned + srcData.drowned
+	newData.maxWinStreak = math.max(destAccountData.maxWinStreak, srcData.maxWinStreak)
+	newData.mapsPlayed = destAccountData.mapsPlayed + srcData.mapsPlayed
+	newData.mapsBought = destAccountData.mapsBought + srcData.mapsBought
+	-- mapsRated are set later
+#if(DM_STATS) then
+	newData.huntersTaken = destAccountData.huntersTaken + srcData.huntersTaken
+	newData.dmVictories = destAccountData.dmVictories + srcData.dmVictories
+	newData.dmPlayed = destAccountData.dmPlayed + srcData.dmPlayed
+#end
+#if(DD_STATS) then
+	newData.ddVictories = destAccountData.ddVictories + srcData.ddVictories
+	newData.ddPlayed = destAccountData.ddPlayed + srcData.ddPlayed
+#end
+#if(RACE_STATS) then
+	newData.raceVictories = destAccountData.raceVictories + srcData.raceVictories
+	newData.racesFinished = destAccountData.racesFinished + srcData.racesFinished
+	newData.racesPlayed = destAccountData.racesPlayed + srcData.racesPlayed
+#end
+	
+	-- Join Msg
+	if(destAccountData.joinmsg == '' and srcData.joinmsg ~= '') then
+		newData.joinmsg = srcData.joinmsg
+	end
+	
+	-- Rates
+	local rows = DbQuery('SELECT r1.map FROM '..RatesTable..' r1, '..RatesTable..' r2 WHERE r1.player=? AND r2.player=? AND r1.map=r2.map', destId, srcId)
+	local maps = {}
+	local questionMarks = {}
+	for i, data in ipairs(rows) do
+		table.insert(maps, data.map)
+		table.insert(questionMarks, '?')
+	end
+	if(#maps > 0) then
+		local questionMarksStr = table.concat(questionMarks, ',')
+		DbQuery('DELETE FROM '..RatesTable..' WHERE player=? AND map IN ('..questionMarksStr..')', srcId, unpack(maps)) -- remove duplicates
+	end
+	DbQuery('UPDATE '..RatesTable..' SET player=? WHERE player=?', destId, srcId) -- set new rates owner
+	local rows = DbQuery('SELECT COUNT(map) AS c FROM '..RatesTable..' WHERE player=?', destId)
+	newData.mapsRated = rows[1].c
+	
+	-- Best times
+	if(BestTimesTable) then
+		local rows = DbQuery('SELECT bt1.map, bt1.time AS time1, bt2.time AS time2 FROM '..BestTimesTable..' bt1, '..BestTimesTable..' bt2 WHERE bt1.player=? AND bt2.player=? AND bt1.map=bt2.map', destId, srcId)
+		local mapsSrc, mapsDst = {}, {}
+		local questionMarksSrc, questionMarksDst = {}, {}
+		newData.toptimes_count = destAccountData.toptimes_count + srcData.toptimes_count
+		
+		for i, data in ipairs(rows) do
+			local delTime = math.max(data.time1, data.time2)
+			if(data.time1 > data.time2) then -- old besttime was better
+				table.insert(mapsDst, data.map)
+				table.insert(questionMarksDst, '?')
+			else -- new besttime is better
+				table.insert(mapsSrc, data.map)
+				table.insert(questionMarksSrc, '?')
+			end
+			
+			local rows = DbQuery('SELECT COUNT(player) AS pos FROM '..BestTimesTable..' WHERE map=? AND time<=?', data.map, delTime)
+			if(rows[1].pos <= 3) then
+				newData.toptimes_count = newData.toptimes_count - 1
+			end
+		end
+		if(#mapsDst > 0) then
+			local questionMarksStr = table.concat(questionMarksDst, ',')
+			BtDeleteTimes('player=? AND map IN ('..questionMarksStr..')', destId, unpack(mapsDst)) -- remove duplicates
+		end
+		if(#mapsSrc > 0) then
+			local questionMarksStr = table.concat(questionMarksSrc, ',')
+			BtDeleteTimes('player=? AND map IN ('..questionMarksStr..')', srcId, unpack(mapsSrc)) -- remove duplicates
+		end
+		DbQuery('UPDATE '..BestTimesTable..' SET player=? WHERE player=?', destId, srcId) -- set new best times owner
+	end
+	
+	-- Profile fields
+	local rows = DbQuery('SELECT p1.field FROM '..ProfilesTable..' p1, '..ProfilesTable..' p2 WHERE p1.player=? AND p2.player=? AND p1.field=p2.field', destId, srcId)
+	local fields = {}
+	local questionMarks = {}
+	for i, data in ipairs(rows) do
+		table.insert(fields, data.field)
+		table.insert(questionMarks, '?')
+	end
+	local questionMarksStr = table.concat(questionMarks, ',')
+	DbQuery('DELETE FROM '..ProfilesTable..' WHERE player=? AND field IN ('..questionMarksStr..')', srcId, unpack(fields)) -- remove duplicates
+	DbQuery('UPDATE '..ProfilesTable..' SET player=? WHERE player=?', destId, srcId) -- set new profile fields owner
+	
+	-- Set new account data and delete old account
+	destAccountData:set(newData, true)
+	DbQuery('DELETE FROM '..PlayersTable..' WHERE player=?', srcId)
+	
+	-- Invalidate achievements cache
+	local player = Player.fromId(destId)
+	if(player) then
+		AchvInvalidateCache(player.el)
+	end
+end
+
 CmdMgr.register{
 	name = 'mergeaccounts',
 	aliases = {'mergeacc'},
 	accessRight = AccessRight('mergeaccounts'),
 	args = {
-		{'Player', type = 'player'},
-		{'OtherAccountID', type = 'int', min = 0},
+		{'DestinationPlayer', type = 'player'},
+		{'SourceAccountID', type = 'int', min = 0},
 	},
-	func = function(ctx, player, id)
-		if(not player.id) then
-			privMsg(ctx.player, "Player %s is not logged in!", player:getName())
+	func = function(ctx, dstPlayer, srcId)
+		if(not dstPlayer.id) then
+			privMsg(ctx.player, "Player %s is not logged in!", dstPlayer:getName())
 			return
 		end
 		
-		if(player.id == id) then
-			privMsg(ctx.player, "Cannot merge account with the same account!", id)
+		if(dstPlayer.id == srcId) then
+			privMsg(ctx.player, "Cannot merge account with the same account!")
 			return
 		end
 		
-		-- get statistics for id
-		local srcAccountData = AccountData.create(id)
-		local src_data = srcAccountData:getTbl()
-		if(not src_data) then
-			privMsg(ctx.player, "Cannot find account %u!", id)
+		local err = mergeAccounts(dstPlayer.id, srcId)
+		if(err) then
+			privMsg(ctx.player, "Failed to merge accounts: %s", err)
+		else
+			scriptMsg("Accounts has been merged. Old account has been removed...")
+		end
+	end
+}
+
+CmdMgr.register{
+	name = 'mergeaccountsoffline',
+	accessRight = AccessRight('mergeaccounts'),
+	args = {
+		{'DestinationAccountName', type = 'str'},
+		{'SourceAccountID', type = 'int', min = 0},
+	},
+	func = function(ctx, dstAccName, srcId)
+		local rows = DbQuery('SELECT player FROM '..PlayersTable..' WHERE account=?', dstAccName)
+		local dstId = rows and rows[1] and rows[1].player
+		if(not dstId) then
+			privMsg(ctx.player, "Cannot find account %s!", dstAccName)
 			return
 		end
 		
-		--[[if(src_data.first_visit > player.accountData.first_visit) then
-			privMsg(ctx.player, "Account for merge is newer")
+		if(dstId == srcId) then
+			privMsg(ctx.player, "Cannot merge account with the same account!")
 			return
-		end]]
-		
-		-- remove duplicated names
-		local names = {}
-		local questionMarks = {}
-		local rows = DbQuery('SELECT n1.name FROM '..NamesTable..' n1, '..NamesTable..' n2 WHERE n1.player=? AND n2.player=? AND n1.name=n2.name', player.id, id)
-		for i, data in ipairs(rows) do
-			table.insert(names, data.name)
-			table.insert(questionMarks, '?')
 		end
 		
-		local questionMarksStr = table.concat(questionMarks, ',')
-		DbQuery('DELETE FROM '..NamesTable..' WHERE player=? AND name IN ('..questionMarksStr..')', id, unpack(names)) -- remove duplicates
-		DbQuery('UPDATE '..NamesTable..' SET player=? WHERE player=?', player.id, id) -- change all names owner
-		
-		-- update stats
-		local newData = {}
-		newData.cash = player.accountData.cash + src_data.cash
-		newData.points = player.accountData.points + src_data.points
-		newData.bidlvl = math.max(src_data.bidlvl, player.accountData.bidlvl)
-		newData.time_here = player.accountData.time_here + src_data.time_here
-		newData.first_visit = src_data.first_visit
-		if(player.accountData.email == '') then
-			newData.email = src_data.email
+		local err = mergeAccounts(dstId, srcId)
+		if(err) then
+			privMsg(ctx.player, "Failed to merge accounts: %s", err)
+		else
+			scriptMsg("Accounts has been merged. Old account has been removed...")
 		end
-		
-		-- Statistics
-		newData.exploded = player.accountData.exploded + src_data.exploded
-		newData.drowned = player.accountData.drowned + src_data.drowned
-		newData.maxWinStreak = math.max(player.accountData.maxWinStreak, src_data.maxWinStreak)
-		newData.mapsPlayed = player.accountData.mapsPlayed + src_data.mapsPlayed
-		newData.mapsBought = player.accountData.mapsBought + src_data.mapsBought
-		-- mapsRated are set later
-#if(DM_STATS) then
-		newData.huntersTaken = player.accountData.huntersTaken + src_data.huntersTaken
-		newData.dmVictories = player.accountData.dmVictories + src_data.dmVictories
-		newData.dmPlayed = player.accountData.dmPlayed + src_data.dmPlayed
-#end
-#if(DD_STATS) then
-		newData.ddVictories = player.accountData.ddVictories + src_data.ddVictories
-		newData.ddPlayed = player.accountData.ddPlayed + src_data.ddPlayed
-#end
-#if(RACE_STATS) then
-		newData.raceVictories = player.accountData.raceVictories + src_data.raceVictories
-		newData.racesFinished = player.accountData.racesFinished + src_data.racesFinished
-		newData.racesPlayed = player.accountData.racesPlayed + src_data.racesPlayed
-#end
-		
-		-- Join Msg
-		if(player.accountData.joinmsg == '' and src_data.joinmsg ~= '') then
-			newData.joinmsg = src_data.joinmsg
-		end
-		
-		-- Rates
-		local rows = DbQuery('SELECT r1.map FROM '..RatesTable..' r1, '..RatesTable..' r2 WHERE r1.player=? AND r2.player=? AND r1.map=r2.map', player.id, id)
-		local maps = {}
-		local questionMarks = {}
-		for i, data in ipairs(rows) do
-			table.insert(maps, data.map)
-			table.insert(questionMarks, '?')
-		end
-		if(#maps > 0) then
-			local questionMarksStr = table.concat(questionMarks, ',')
-			DbQuery('DELETE FROM '..RatesTable..' WHERE player=? AND map IN ('..questionMarksStr..')', id, unpack(maps)) -- remove duplicates
-		end
-		DbQuery('UPDATE '..RatesTable..' SET player=? WHERE player=?', player.id, id) -- set new rates owner
-		local rows = DbQuery('SELECT COUNT(map) AS c FROM '..RatesTable..' WHERE player=?', player.id)
-		newData.mapsRated = rows[1].c
-		
-		-- Best times
-		if(BestTimesTable) then
-			local rows = DbQuery('SELECT bt1.map, bt1.time AS time1, bt2.time AS time2 FROM '..BestTimesTable..' bt1, '..BestTimesTable..' bt2 WHERE bt1.player=? AND bt2.player=? AND bt1.map=bt2.map', player.id, id)
-			local mapsSrc, mapsDst = {}, {}
-			local questionMarksSrc, questionMarksDst = {}, {}
-			newData.toptimes_count = player.accountData.toptimes_count + src_data.toptimes_count
-			
-			for i, data in ipairs(rows) do
-				local delTime = math.max(data.time1, data.time2)
-				if(data.time1 > data.time2) then -- old besttime was better
-					table.insert(mapsDst, data.map)
-					table.insert(questionMarksDst, '?')
-				else -- new besttime is better
-					table.insert(mapsSrc, data.map)
-					table.insert(questionMarksSrc, '?')
-				end
-				
-				local rows = DbQuery('SELECT COUNT(player) AS pos FROM '..BestTimesTable..' WHERE map=? AND time<=?', data.map, delTime)
-				if(rows[1].pos <= 3) then
-					newData.toptimes_count = newData.toptimes_count - 1
-				end
-			end
-			if(#mapsDst > 0) then
-				local questionMarksStr = table.concat(questionMarksDst, ',')
-				BtDeleteTimes('player=? AND map IN ('..questionMarksStr..')', player.id, unpack(mapsDst)) -- remove duplicates
-			end
-			if(#mapsSrc > 0) then
-				local questionMarksStr = table.concat(questionMarksSrc, ',')
-				BtDeleteTimes('player=? AND map IN ('..questionMarksStr..')', id, unpack(mapsSrc)) -- remove duplicates
-			end
-			DbQuery('UPDATE '..BestTimesTable..' SET player=? WHERE player=?', player.id, id) -- set new best times owner
-		end
-		
-		-- Profile fields
-		local rows = DbQuery('SELECT p1.field FROM '..ProfilesTable..' p1, '..ProfilesTable..' p2 WHERE p1.player=? AND p2.player=? AND p1.field=p2.field', player.id, id)
-		local fields = {}
-		local questionMarks = {}
-		for i, data in ipairs(rows) do
-			table.insert(fields, data.field)
-			table.insert(questionMarks, '?')
-		end
-		local questionMarksStr = table.concat(questionMarks, ',')
-		DbQuery('DELETE FROM '..ProfilesTable..' WHERE player=? AND field IN ('..questionMarksStr..')', id, unpack(fields)) -- remove duplicates
-		DbQuery('UPDATE '..ProfilesTable..' SET player=? WHERE player=?', player.id, id) -- set new profile fields owner
-		
-		-- Set new account data and delete old account
-		player.accountData:set(newData, true)
-		DbQuery('DELETE FROM '..PlayersTable..' WHERE player=?', id)
-		AchvInvalidateCache(player.el)
-		
-		scriptMsg("Accounts has been merged. Old account has been removed...")
 	end
 }
 
