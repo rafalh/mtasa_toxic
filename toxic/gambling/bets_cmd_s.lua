a00761ce272781c12bc3f19a76547a02b75e15ea
Rafał Harabień <rafalh1992@o2.pl>
2014-01-26 12:20:46 +0000

    Don't allow betting dead players

--- toxic/gambling/bets_cmd_s.lua
+++ toxic/gambling/bets_cmd_s.lua
@@ -6,19 +6,22 @@ CmdMgr.register{
 		{'player', type = 'player', defValFromCtx = 'player'},
 	},
 	func = function(ctx, cash, targetPlayer)
-		local bet_min_players = Settings.bet_min_players
-		local max_bet = Settings.max_bet * ctx.player.accountData:get('bidlvl')
+		local betMinPlayers = Settings.bet_min_players
+		local maxBet = Settings.max_bet * ctx.player.accountData:get('bidlvl')
+		local targetMap = getCurrentMap(targetPlayer.room)
 		
-		if(g_PlayersCount < bet_min_players) then
-			privMsg(ctx.player, "Not enough players to bet - %u are needed.", bet_min_players)
+		if(g_PlayersCount < betMinPlayers) then
+			privMsg(ctx.player, "Not enough players to bet - %u are needed.", betMinPlayers)
 		elseif(GbAreBetsPlaced()) then
 			privMsg(ctx.player, "Bets are placed!")
 		elseif(ctx.player.accountData:get('cash') < cash) then
 			privMsg(ctx.player, "You do not have enough cash!")
-		elseif(cash > max_bet) then
-			privMsg(ctx.player, "Your maximal bet is %s!", formatNumber(max_bet))
+		elseif(cash > maxBet) then
+			privMsg(ctx.player, "Your maximal bet is %s!", formatNumber(maxBet))
 		elseif(ctx.player.bet) then
 			privMsg(ctx.player, "You already bet %s on %s!", formatMoney(ctx.player.betcash), getPlayerName(ctx.player.bet))
+		elseif(not targetPlayer:isAlive() and targetMap and targetMap:getRespawn()) then
+			privMsg(ctx.player, "%s is already dead!", targetPlayer:getName())
 		else
 			GbBet(ctx.player.el, targetPlayer.el, cash)
 			privMsg(ctx.player, "You bet %s on %s!", formatMoney(cash), targetPlayer:getName())
