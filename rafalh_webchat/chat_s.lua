7c2a5e716de7fc37cb6acb5a480284a06f0e7f90
Rafał Harabień <rafalh1992@o2.pl>
2014-03-21 17:37:44 +0000

    [webchat] Fix formatting

--- rafalh_webchat/chat_s.lua
+++ rafalh_webchat/chat_s.lua
@@ -4,65 +4,54 @@
 
 local g_Msgs = {}
 local g_MaxMsgs = 20
-local g_Root = getRootElement ()
+local g_Root = getRootElement()
 local g_MsgId = 0
 
----------------------------------
--- Local function declarations --
----------------------------------
+---------------------
+-- Local functions --
+---------------------
 
-local getTime
-local onPlayerChatHandler
-local onPlayerJoinHandler
-local onPlayerQuitHandler
-local onPlayerWastedHandler
-local onPlayerChangeNickHandler
-
---------------------------------
--- Local function definitions --
---------------------------------
-
-table.size = function ( tab )
+function table.size(tab)
     local n = 0
-    for v in pairs( tab ) do n = n + 1 end
+    for v in pairs( tab) do n = n + 1 end
     return n
 end
 
-getTime = function ()
-	local time = getRealTime ()
-	return string.format ( "%02d:%02d:%02d", time.hour, time.minute, time.second )
+local function getTime()
+	local time = getRealTime()
+	return string.format('%02d:%02d:%02d', time.hour, time.minute, time.second)
 end
 
-onPlayerChatHandler = function ( msg, type )
-	if ( type == 0 ) then
-		local r, g, b = getPlayerNametagColor ( source )
-		addChatStr ( string.format ( "#%02x%02x%02x", r, g, b )..getPlayerName ( source ).."#ffffff: "..msg )
-	elseif ( type == 1 ) then
-		addChatStr ( "#ff00ff* "..string.gsub ( getPlayerName ( source ), "#%x%x%x%x%x%x", "" ).." "..msg )
+local function onPlayerChatHandler(msg, type)
+	if(type == 0) then
+		local r, g, b = getPlayerNametagColor(source)
+		addChatStr(string.format('#%02x%02x%02x', r, g, b)..getPlayerName(source)..'#ffffff: '..msg)
+	elseif(type == 1) then
+		addChatStr('#ff00ff* '..string.gsub(getPlayerName(source), '#%x%x%x%x%x%x', '')..' '..msg)
 	end
 end
 
-onPlayerJoinHandler = function ( )
-	addChatStr ( "#ff6060* "..getPlayerName ( source ).." joined the game!" )
+local function onPlayerJoinHandler()
+	addChatStr('#ff6060* '..getPlayerName(source)..' joined the game!')
 end
 
-onPlayerQuitHandler = function ( quitType )
-	addChatStr ( "#ff6060* "..string.gsub ( getPlayerName ( source ), "#%x%x%x%x%x%x", "" ).." has left the game ("..quitType..")." )
+local function onPlayerQuitHandler(quitType)
+	addChatStr('#ff6060* '..string.gsub(getPlayerName(source), '#%x%x%x%x%x%x', '')..' has left the game ('..quitType..').')
 end
 
-onPlayerWastedHandler = function ( Ammo, killer, killerWeapon, bodypart )
-	if ( killer and getElementType ( killer ) == "player" ) then
-		addChatStr ( getPlayerName(killer).." killed "..getPlayerName(source).." using weapon "..getWeaponNameFromID(killerWeapon).."."	)
+local function onPlayerWastedHandler(Ammo, killer, killerWeapon, bodypart)
+	if(killer and getElementType(killer) == 'player') then
+		addChatStr(getPlayerName(killer)..' killed '..getPlayerName(source)..' using weapon '..getWeaponNameFromID(killerWeapon)..'.'	)
 	else
-		addChatStr ( "#ff6060* "..string.gsub ( getPlayerName ( source ), "#%x%x%x%x%x%x", "" ).." died." )
+		addChatStr('#ff6060* '..string.gsub(getPlayerName(source), '#%x%x%x%x%x%x', '')..' died.')
 	end
 end
 
-onPlayerChangeNickHandler = function ( oldNick, newNick )
-	oldNick = string.gsub ( oldNick, "#%x%x%x%x%x%x", "" )
-	newNick = string.gsub ( newNick, "#%x%x%x%x%x%x", "" )
+local function onPlayerChangeNickHandler(oldNick, newNick)
+	oldNick = string.gsub(oldNick, '#%x%x%x%x%x%x', '')
+	newNick = string.gsub(newNick, '#%x%x%x%x%x%x', '')
 	if newNick ~= oldNick then
-		addChatStr ( "#ff6060* "..oldNick.." is now known as "..newNick )
+		addChatStr('#ff6060* '..oldNick..' is now known as '..newNick)
 	end
 end
 
@@ -70,74 +59,74 @@ end
 -- Global function definitions --
 ---------------------------------
 
-function getChatMessages ( id ) -- pobiera wszystkie wiadomoúci ktÛre maja id wieksze od id
+function getChatMessages(id) -- pobiera wszystkie wiadomo≈õci kt√≥re maja id wieksze od id
 	local tmp = {}
-	for _, msg in ipairs ( g_Msgs ) do
-		if msg[2] > tonumber ( id ) then
-			table.insert ( tmp, msg )
+	for _, msg in ipairs(g_Msgs) do
+		if msg[2] > tonumber(id) then
+			table.insert(tmp, msg)
 		end
 	end
 	return tmp
 end
 
-function addChatStr ( str )
-	if table.size ( g_Msgs ) >= g_MaxMsgs then
-		table.remove ( g_Msgs, 1 )
+function addChatStr(str)
+	if table.size(g_Msgs) >= g_MaxMsgs then
+		table.remove(g_Msgs, 1)
 	end
-	str = string.gsub ( str, "•", "&#260;" )
-	str = string.gsub ( str, "π", "&#261;" )
-	str = string.gsub ( str, "∆", "&#262;" )
-	str = string.gsub ( str, "Ê", "&#263;" )
-	str = string.gsub ( str, " ", "&#280;" )
-	str = string.gsub ( str, "Í", "&#281;" )
-	str = string.gsub ( str, "£", "&#321;" )
-	str = string.gsub ( str, "≥", "&#322;" )
-	str = string.gsub ( str, "—", "&#323;" )
-	str = string.gsub ( str, "Ò", "&#324;" )
-	str = string.gsub ( str, "”", "&#211;" )
-	str = string.gsub ( str, "Û", "&#243;" )
-	str = string.gsub ( str, "å", "&#346;" )
-	str = string.gsub ( str, "ú", "&#347;" )
-	str = string.gsub ( str, "è", "&#377;" )
-	str = string.gsub ( str, "ü", "&#378;" )
-	str = string.gsub ( str, "Ø", "&#379;" )
-	str = string.gsub ( str, "ø", "&#380;" )
-	table.insert ( g_Msgs, { [0]=getTime (), [1]=str, [2]=g_MsgId } )
+	str = string.gsub(str, 'ƒÑ', '&#260;')
+	str = string.gsub(str, 'ƒÖ', '&#261;')
+	str = string.gsub(str, 'ƒÜ', '&#262;')
+	str = string.gsub(str, 'ƒá', '&#263;')
+	str = string.gsub(str, 'ƒò', '&#280;')
+	str = string.gsub(str, 'ƒô', '&#281;')
+	str = string.gsub(str, '≈Å', '&#321;')
+	str = string.gsub(str, '≈Ç', '&#322;')
+	str = string.gsub(str, '≈É', '&#323;')
+	str = string.gsub(str, '≈Ñ', '&#324;')
+	str = string.gsub(str, '√ì', '&#211;')
+	str = string.gsub(str, '√≥', '&#243;')
+	str = string.gsub(str, '≈ö', '&#346;')
+	str = string.gsub(str, '≈õ', '&#347;')
+	str = string.gsub(str, '≈π', '&#377;')
+	str = string.gsub(str, '≈∫', '&#378;')
+	str = string.gsub(str, '≈ª', '&#379;')
+	str = string.gsub(str, '≈º', '&#380;')
+	table.insert(g_Msgs, { [0]=getTime(), [1]=str, [2]=g_MsgId })
 	g_MsgId = g_MsgId + 1
 end
 
-function sendChatMsg ( user, name, msg )
-	msg = string.gsub ( msg, "\196\132", "•" )
-	msg = string.gsub ( msg, "\196\133", "π" )
-	msg = string.gsub ( msg, "\196\134", "∆" )
-	msg = string.gsub ( msg, "\196\135", "Ê" )
-	msg = string.gsub ( msg, "\196\152", " " )
-	msg = string.gsub ( msg, "\196\153", "Í" )
-	msg = string.gsub ( msg, "\197\129", "£" )
-	msg = string.gsub ( msg, "\197\130", "≥" )
-	msg = string.gsub ( msg, "\197\131", "—" )
-	msg = string.gsub ( msg, "\197\132", "Ò" )
-	msg = string.gsub ( msg, "\195\147", "”" )
-	msg = string.gsub ( msg, "\195\179", "Û" )
-	msg = string.gsub ( msg, "\197\154", "å" )
-	msg = string.gsub ( msg, "\197\155", "ú" )
-	msg = string.gsub ( msg, "\197\185", "è" )
-	msg = string.gsub ( msg, "\197\186", "ü" )
-	msg = string.gsub ( msg, "\197\187", "Ø" )
-	msg = string.gsub ( msg, "\197\188", "ø" )
-	local str = "#ffff00"..tostring ( name ).." (web)#ffffff: "..tostring ( msg )
-	outputChatBox ( str, g_Root, 255, 255, 255, true )
-	addChatStr ( str )
-	--outputChatBox ( tostring ( getAccountName ( user ) ) )
-	--exports.toxic:chatHandler ( user, msg, 0 )
+function sendChatMsg(user, name, msg)
+	msg = string.gsub(msg, '\196\132', 'ƒÑ')
+	msg = string.gsub(msg, '\196\133', 'ƒÖ')
+	msg = string.gsub(msg, '\196\134', 'ƒÜ')
+	msg = string.gsub(msg, '\196\135', 'ƒá')
+	msg = string.gsub(msg, '\196\152', 'ƒò')
+	msg = string.gsub(msg, '\196\153', 'ƒô')
+	msg = string.gsub(msg, '\197\129', '≈Å')
+	msg = string.gsub(msg, '\197\130', '≈Ç')
+	msg = string.gsub(msg, '\197\131', '≈É')
+	msg = string.gsub(msg, '\197\132', '≈Ñ')
+	msg = string.gsub(msg, '\195\147', '√ì')
+	msg = string.gsub(msg, '\195\179', '√≥')
+	msg = string.gsub(msg, '\197\154', '≈ö')
+	msg = string.gsub(msg, '\197\155', '≈õ')
+	msg = string.gsub(msg, '\197\185', '≈π')
+	msg = string.gsub(msg, '\197\186', '≈∫')
+	msg = string.gsub(msg, '\197\187', '≈ª')
+	msg = string.gsub(msg, '\197\188', '≈º')
+	local str = '#ffff00'..tostring(name)..' (web)#ffffff: '..tostring(msg)
+	outputChatBox(str, g_Root, 255, 255, 255, true)
+	addChatStr(str)
+	--outputChatBox(tostring(getAccountName(user)))
+	--exports.toxic:chatHandler(user, msg, 0)
 end
 
 ------------
 -- Events --
 ------------
 
-addEventHandler ( "onPlayerChat", g_Root, onPlayerChatHandler )
-addEventHandler ( "onPlayerJoin", g_Root, onPlayerJoinHandler )
-addEventHandler ( "onPlayerQuit", g_Root, onPlayerQuitHandler )
-addEventHandler ( "onPlayerWasted", g_Root, onPlayerWastedHandler )
-addEventHandler ( "onPlayerChangeNick", g_Root, onPlayerChangeNickHandler )
+addEventHandler('onPlayerChat', g_Root, onPlayerChatHandler)
+addEventHandler('onPlayerJoin', g_Root, onPlayerJoinHandler)
+addEventHandler('onPlayerQuit', g_Root, onPlayerQuitHandler)
+addEventHandler('onPlayerWasted', g_Root, onPlayerWastedHandler)
+addEventHandler('onPlayerChangeNick', g_Root, onPlayerChangeNickHandler)
--- rafalh_webchat/script.js
+++ rafalh_webchat/script.js
@@ -1,101 +1,101 @@
 var last_msg = -1;
 
-function js_updateChat ()
+function js_updateChat()
 {
-	getChatMessages ( last_msg.toString (),
-		function ( msgs )
+	getChatMessages(last_msg.toString(),
+		function(msgs)
 		{
-			var table = document.getElementById ( "msgs" );
+			var table = document.getElementById("msgs");
 			var c = msgs ? msgs.length : 0;
 			
-			for ( var i = 0; i < c; ++i )
+			for(var i = 0; i < c; ++i)
 			{
 				var id = msgs[i][2].toString();
-				if ( id > last_msg )
+				if(id > last_msg)
 				{
 					last_msg = id;
 				
-					var row = document.createElement ( "tr" );
+					var row = document.createElement("tr");
 					
 					var buf = msgs[i][1].toString().htmlEntities();
 					buf = buf.replace(/(#[0-9abcdef]{6})/gi, "</span><span style=\"color:$1\">");
 					
-					var cell = document.createElement ( "td" );
+					var cell = document.createElement("td");
 					cell.className = "chattime";
 					cell.innerHTML = "[" + msgs[i][0].toString() + "]";
-					row.appendChild ( cell );
+					row.appendChild(cell);
 					
-					var cell2 = document.createElement ( "td" );
+					var cell2 = document.createElement("td");
 					cell2.innerHTML = "<span class=\"chatmsg\">" + buf + "</span>";
 					cell2.className = "chatmsg";
-					row.appendChild ( cell2 );
+					row.appendChild(cell2);
 					
-					table.appendChild ( row );
+					table.appendChild(row);
 				}
 			}
 			
-			while ( table.children.length > 20 )
-				table.removeChild ( table.firstChild );
+			while(table.children.length > 20)
+				table.removeChild(table.firstChild);
 		}
 	);
 }
 
-function js_updateChatTimer ()
+function js_updateChatTimer()
 {
-	js_updateChat ();
-	setTimeout ( "js_updateChatTimer()", 500 );
+	js_updateChat();
+	setTimeout("js_updateChatTimer()", 500);
 }
 
-String.prototype.htmlEntities = function ()
+String.prototype.htmlEntities = function()
 {
 	return this.replace(/</g,'&lt;').replace(/>/g,'&gt;');
 };
 
-function js_sendChatMsg ()
+function js_sendChatMsg()
 {
-	var nick = document.getElementById ( "nick" );
-	var msg = document.getElementById ( "msg" );
+	var nick = document.getElementById("nick");
+	var msg = document.getElementById("msg");
 		
-	if ( nick.value == "Nick" || nick.value == "" )
+	if(nick.value == "Nick" || nick.value == "")
 	{
-		alert ( "Type a nick!" );
+		alert("Type a nick!");
 		return 0;
 	}
 		
-	if ( msg.value == "Message" || msg.value == "" )
+	if(msg.value == "Message" || msg.value == "")
 	{
-		alert ( "Type your message to send!" );
+		alert("Type your message to send!");
 		return 0;
 	}
 	
-	sendChatMsg ( "", nick.value, msg.value, function ( r ) { } );
+	sendChatMsg("", nick.value, msg.value, function(r) { });
 	msg.value = '';
 }
 
-function js_nickB ()
+function js_nickB()
 {
-	var nick = document.getElementById( 'nick' );
-	if ( nick.value=='' ) 
+	var nick = document.getElementById( 'nick');
+	if(nick.value=='') 
 		nick.value='Nick';
 }
 
-function js_nickF ()
+function js_nickF()
 {
-	var nick = document.getElementById( 'nick' ); 
-	if ( nick.value=='Nick' )
+	var nick = document.getElementById( 'nick'); 
+	if(nick.value=='Nick')
 		nick.value='';
 }
 
-function js_msgB ()
+function js_msgB()
 {
-	var msg = document.getElementById( 'msg' );	
-	if ( msg.value == '' ) 
+	var msg = document.getElementById( 'msg');	
+	if(msg.value == '') 
 		msg.value = 'Message';
 }
 
-function js_msgF ()
+function js_msgF()
 {
-	var msg = document.getElementById( 'msg' );
-	if ( msg.value == 'Message' )
+	var msg = document.getElementById( 'msg');
+	if(msg.value == 'Message')
 		msg.value = '';
 }
